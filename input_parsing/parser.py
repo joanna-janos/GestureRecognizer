007c09903c379bab22a6f21c601d01ee6c58c1eb
joanna-janos <janos.joanna@gmail.com>
2020-01-10 23:51:56 +0100

    Add argument to provide by user: number of epochs, path where model should be saved and value indicating what part of model should be trained

--- input_parsing/parser.py
+++ input_parsing/parser.py
@@ -10,19 +10,30 @@ def parse_arguments():
                         default='data/',
                         help='Path to data directory')
 
+    parser.add_argument('--path_to_saved_model',
+                        type=str,
+                        default='trained_model/',
+                        help='Path where trained model will be stored')
+
     parser.add_argument('--batch_size',
                         type=int,
                         default=64,
                         help='Size of batch')
+
+    parser.add_argument('--epochs',
+                        type=int,
+                        default=10,
+                        help='Number of epochs')
 
     parser.add_argument('--model_name',
                         type=str,
                         default='MnasNet',
                         help='Model name. The only available are: MnasNet, SqueezeNet and MobileNetV2.')
 
-    parser.add_argument('--epochs',
-                        type=int,
-                        default=50,
-                        help='Number of epochs')
+    parser.add_argument('--train_only_last_layer',
+                        type=bool,
+                        default=True,
+                        help='True if pretrained model should have only last layer learnable,'
+                             'False if all weights should be adjusted during training')
 
     return parser.parse_args()
