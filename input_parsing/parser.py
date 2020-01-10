9726504036841f68f86c305f3b542b180357824b
joanna-janos <janos.joanna@gmail.com>
2020-01-10 18:15:19 +0100

    Add new argument (batch size) to provide by user

--- input_parsing/parser.py
+++ input_parsing/parser.py
@@ -5,4 +5,5 @@ def parse_arguments():
     """ Parse user provided arguments. """
     parser = argparse.ArgumentParser(description='Gesture recognizer')
     parser.add_argument('--data_path', type=str, default='data/', help='Path to data directory')
+    parser.add_argument('--batch_size', type=int, default=64, help='Size of batch')
     return parser.parse_args()
